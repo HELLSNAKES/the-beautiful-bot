01190f86b73f449694a57a2f58637d3d76d3d801
MooradAltamimi <moorad2003@gmail.com>
2019-11-29 13:40:32 +0000

    Added a basic $c command

--- index.js
+++ index.js
@@ -152,6 +152,16 @@ client.on('message', async msg => {
 			getRepoData(msg);
 		} else if (command == 'c') {
 			getComparablePlay(msg);
+		} else if (command == 'dummy') {
+			msg.channel.send({ embed: {
+				"url": "https://discordapp.com",
+				"color": 14869524,
+				"author": {
+				  "name": ".",
+				  "url": "https://osu.ppy.sh/beatmapsets/952393#osu/2074851"
+				}
+			}
+			  });
 		}
 	}
 	if (msg.content === 'bot you alive?') { // bot are you alive
@@ -542,7 +552,7 @@ function recent(msg, user) {
 			errorMessage(msg, 4044);
 			return;
 		}
-
+		
 		const grade = client.emojis.find(emoji => emoji.name === 'grade_' + body[0].rank.toLowerCase());
 		request(`https://osu.ppy.sh/api/get_beatmaps?k=${process.env.osuAPI}&b=${body[0].beatmap_id}`, {
 			json: true
@@ -860,13 +870,120 @@ function errorMessage(msg, errCode) {
 }
 
 function getComparablePlay(msg) {
+	var done = false;
 	msg.channel.fetchMessages()
-		.then(messages => {console.log(messages.size);messages.forEach((x) => {
-		
-			msg.channel.fetchMessage(x.id).then(z => {
-				console.log(z.embeds[0].url)
-				console.log(z.embeds[0].author.name.includes('Here are the top 5 plays for') ? 'top' : 'recent')
+		.then(messages => messages.forEach((message) => {
+			if (message.author.id != 637205590652747778 || done) {
+				return;
+			}
+			msg.channel.fetchMessage(message.id).then(fetchedMessage => {
+
+				console.log(fetchedMessage.embeds[0].author.url)
+				if (fetchedMessage.embeds[0].author.name.includes('Here are the top 5 plays for')) {
+					sendCompareEmbed(msg, 0, fetchedMessage.embeds[0].description, msg.author.id);
+				} else {
+					console.log('recent')
+					sendCompareEmbed(msg, 1, fetchedMessage.embeds[0].author.url, msg.author.id);
+
+				}
+
 			}).catch(() => console.log(undefined))
-		})})
+			done = true
+		}))
 		.catch(console.error);
+}
+
+function sendCompareEmbed(msg, playType, content, userid) {
+	readDB(msg, userid, (doc) => {
+		console.log(doc.osuUsername)
+		if (playType == 1) {
+			request(`https://osu.ppy.sh/api/get_scores?k=${process.env.osuAPI}&u=${doc.osuUsername}&b=${content.slice(content.indexOf('#osu/')+5)}`, (err, res, body) => {
+				body = JSON.parse(body)
+
+				
+				request(`https://osu.ppy.sh/api/get_beatmaps?k=${process.env.osuAPI}&b=${content.slice(content.indexOf('#osu/')+5)}`, {
+					json: true
+				}, (err, res, beatmapData) => {
+					if (body.length == 0) {
+						msg.channel.send(`Sorry but I couldn't find any plays on \`${beatmapData[0].title} [${beatmapData[0].version}].\``);
+						return;
+					}
+					var grade = client.emojis.find(emoji => emoji.name === 'grade_' + body[0].rank.toLowerCase());
+					var accuracy = (50 * parseInt(body[0].count50) + 100 * parseInt(body[0].count100) + 300 * parseInt(body[0].count300)) / (300 * (parseInt(body[0].count50) + parseInt(body[0].count100) + parseInt(body[0].count300) + parseInt(body[0].countmiss)));
+					accuracy = Math.floor(accuracy * 10000) / 100;
+
+					exec(`curl -s https://osu.ppy.sh/osu/${content.slice(content.indexOf('#osu/')+5)} | node pp.js +${getMods(body[0].enabled_mods)} ${accuracy}% ${body[0].maxcombo}x ${body[0].countmiss}m`, (err, stdout, stderr) => {
+						if (err) {
+							// node couldn't execute the command
+							return;
+						}
+
+						// the *entire* stdout and stderr (buffered)
+						var ppAndDiff = stdout.replace('\n', '').split('$')
+						console.log(ppAndDiff)
+						console.log(`stdout: ${stdout}`);
+						console.log(`stderr: ${stderr}`);
+
+
+						var datebefore = new Date(body[0].date);
+						var datenow = new Date();
+						datenow = new Date(datenow.getUTCFullYear(), datenow.getUTCMonth(), datenow.getUTCDate(), datenow.getUTCHours(), datenow.getUTCMinutes(), datenow.getUTCSeconds(), datenow.getUTCMilliseconds());
+						console.log(datenow);
+
+						var difference = new Date(datenow - datebefore);
+						var formattedDate = `${difference.getMonth() == 0 ? '' : ' ' + difference.getMonth() + ' Months'}${difference.getDate() - 1 == 0 ? '' : ' ' + difference.getDate() - 1 + ' Days'}${difference.getHours() - 1 == 0 ? '' : ' ' + difference.getHours() - 1 + ' Hours'}${difference.getMinutes() == 0 ? '' : ' ' + difference.getMinutes() + ' Minutes'}${difference.getSeconds() == 0 ? '' : ' ' + difference.getSeconds() + ' Seconds'} ago`;
+						console.log(formattedDate);
+
+						let mods = getMods(body[0].enabled_mods);
+						var colour = 0;
+						if (body[0].rank.toLowerCase() == 'f' || body[0].rank.toLowerCase() == 'd') colour = 15158332;
+						else if (body[0].rank.toLowerCase() == 'c') colour = 10181046;
+						else if (body[0].rank.toLowerCase() == 'b') colour = 3447003;
+						else if (body[0].rank.toLowerCase() == 'a') colour = 3066993;
+						else if (body[0].rank.toLowerCase() == 's') colour = 15844367;
+						else if (body[0].rank.toLowerCase() == 'sh') colour = 12370112;
+						else if (body[0].rank.toLowerCase() == 'x') colour = 16580705;
+						else if (body[0].rank.toLowerCase() == 'xh') colour = 16580705;
+
+						var completion = 0;
+						if (body[0].rank.toLowerCase() == 'f') {
+							completion = Math.floor((parseInt(body[0].count50) + parseInt(body[0].count100) + parseInt(body[0].count300) + parseInt(body[0].countmiss)) / parseInt(ppAndDiff[2]) * 10000) / 100;
+						}
+
+						if (!mods.includes('DT') && !mods.includes('HR') && !mods.includes('EZ') && !mods.includes('HT')) {
+							ppAndDiff[1] = Math.floor(beatmapData[0].difficultyrating * 100) / 100;
+						}
+
+						var accuracy = (50 * parseInt(body[0].count50) + 100 * parseInt(body[0].count100) + 300 * parseInt(body[0].count300)) / (300 * (parseInt(body[0].count50) + parseInt(body[0].count100) + parseInt(body[0].count300) + parseInt(body[0].countmiss)));
+						accuracy = Math.floor(accuracy * 10000) / 100;
+
+						const embed = {
+							'description': `${grade} - **${body[0].pp}pp** - ${accuracy}%${body[0].perfect == 1 ? ' - __**[Full Combo!]**__' : ''}\n${'★'.repeat(Math.floor(beatmapData[0].difficultyrating))} **[${Math.floor(beatmapData[0].difficultyrating * 100)/100}★]${ppAndDiff[1] != Math.floor(beatmapData[0].difficultyrating * 100)/100 ? ` (${ppAndDiff[1]}★ with Mods)` : ''}**\nCombo: **x${format(body[0].maxcombo)}/x${format(beatmapData[0].max_combo)}**	Score: **${format(body[0].score)}**\n[${body[0].count300}/${body[0].count100}/${body[0].count50}/${body[0].countmiss}]${body[0].rank.toLowerCase() == 'f' ? `\nCompleted: **${completion}%**` :''}`, //\nAchieved: **${formattedDate}**
+							'url': 'https://discordapp.com',
+							'color': colour,
+							'thumbnail': {
+								'url': `https://b.ppy.sh/thumb/${beatmapData[0].beatmapset_id}.jpg`
+							},
+							'author': {
+								'name': `${beatmapData[0].title} [${beatmapData[0].version}] +${getMods(body[0].enabled_mods)}`,
+								'url': `https://osu.ppy.sh/beatmapsets/${beatmapData[0].beatmapset_id}#osu/${beatmapData[0].beatmap_id}`,
+								'icon_url': `https://a.ppy.sh/${body[0].user_id}?1566997187.jpeg`
+							},
+							'footer': {
+								'icon_url': 'https://cdn.discordapp.com/avatars/647218819865116674/30bf8360b8a5adef5a894d157e22dc34.png?size=128',
+								'text': 'Always Remember, The beautiful bot loves you <3'
+							}
+						};
+						msg.channel.send({
+							embed
+						});
+
+					});
+
+
+				});
+
+			});
+		}
+	});
 }
